58c35abcbb013d863c447c5570ad2ec21a7a2866
Alwa$t <Alwa$t@DESKTOP-FP7F3LT>
2021-12-03 17:16:35 +0200

    Добавьте файлы проекта.

---
+++ WpfBindingProducts/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingProducts
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Products> Product { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Product = new List<Products>
+            {
+                new Products{Name = "Мандарины", Price=150.5, Amount = 105.2, Really = 100, Barcode = 10201342, Bought = true},
+                new Products{Name = "Бананы", Price=300.9, Amount = 5.3, Really = 99, Barcode = 65423432, Bought = false},
+                new Products{Name = "Апельсины", Price=1000.5, Amount = 2.2, Really = 5, Barcode = 5646546, Bought = true}
+            };
+        }
+
+        private void gridProducts_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListBox.ItemsSource = Product;
+        }
+        
+    }
+}
---
+++ WpfBindingProducts/Products.cs
@@ -0,0 +1,17 @@
+﻿namespace WpfBindingProducts
+{
+    public class Products
+    {
+        public string Name { set; get; } 
+        public double Price { set; get; }
+        public double Amount { set; get; }
+        public double Really { set; get; }
+        public long Barcode { set; get; }
+        public bool Bought { set; get; }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
